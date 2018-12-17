79697161cd6239a24eef178d44516b896f85c5b4
Maurii19 <mdiazar17dw@ikzubirimanteo.com>
2018-12-17 09:02:30 +0100

    NavBar fixed

--- database/migrations/2018_11_09_120422_social_pets.php
+++ database/migrations/2018_11_09_120422_social_pets.php
@@ -29,6 +29,6 @@ class SocialPets extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('contact');
     }
 }
--- resources/views/layouts/navbarLoginRegister.blade.php
+++ resources/views/layouts/navbarLoginRegister.blade.php
@@ -1,4 +1,5 @@
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <!-- NavBar -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
           @if(Session::has('success-message'))
     <div class="alert alert-success"> 
         <button type="button" 
@@ -14,8 +15,9 @@
           Menu
           <i class="fas fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav mr-auto navbar">
+        <div class="collapse navbar-collapse" id="navbarResponsive"> 
+        <ul class="navbar-nav mr-auto navbar">
+          @guest
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#socialpets">¿Qué es Social Pets?</a>
             </li>
@@ -26,7 +28,39 @@
               <a class="nav-link js-scroll-trigger" href="#contacto">Contacto</a>
             </li>
           </ul>
-        @include('layouts.navbarLoginRegister')
+          
+           <ul class="navbar-nav mr navbar">
+           <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#" data-toggle="modal" data-target="#loginModal">Login <i class="fas fa-sign-in-alt"></i></a>
+            </li>
+           <li class="nav-item">
+             @if (Route::has('register'))
+              <a class="nav-link js-scroll-trigger" href="#" data-toggle="modal" data-target="#registroModal">Registro <i class="fas fa-user-circle"></i></a>
+             @endguest
+            </li>
+            @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                      document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+            @endif
+            
+</ul>
+
+
         </div>
       </div>
     </nav>
