ec88e60caed6e697c81727bcbbf662913ed67590
jgonzalezma <jgonzalezma17dw@ikzubirimanteo.com>
2019-01-14 09:00:12 +0100

    heroku email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,17 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verifica tu correo electrónico') }}</div>
+                <div class="card-header">{{ __('Verifica tu email') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('Se ha enviado un correo a tu bandeja de entrada.') }}
+                            {{ __('Se ha enviado un link de verificacion a tu correo') }}
                         </div>
                     @endif
 
                     {{ __('Para poder acceder a las funcionalidades de la página, porfavor, verifica tu correo electrónico.') }}
-                    {{ __('Si no has recibido ningún correo') }}, <a href="{{ route('verification.resend') }}">{{ __('Haz click aquí para recibir otro') }}</a>.
+                    {{ __('Si no has recibido ningún correo') }}, <a href="{{ route('verification.resend') }}">{{ __('click aqui para solicitar otro') }}</a>.
                 </div>
             </div>
         </div>
