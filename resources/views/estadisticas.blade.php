4bd2a696f58a862fc92a5d44e5c0259ecd25c3c4
davidzabalperez <davidzabalperez@gmail.com>
2019-01-17 12:24:44 +0100

    estadistica casi

--- resources/views/estadisticas.blade.php
+++ resources/views/estadisticas.blade.php
@@ -20,13 +20,8 @@
     <link href="fontawesome-free/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-
-
+    <script src="/js/Chart.min.js"></script>
   </head>
-
-
 
   <body id="page-top">
     <!-- NavBar -->
@@ -34,27 +29,65 @@
 <section id="socialpets" class="about-section text-center">
 
 <div class="container">
- 
-</div>
+    
+    <canvas id="myChart" width="100%" height="40%"></canvas>
+    <script>
+    var ctx = document.getElementById("myChart"); 
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [
+                @foreach($usersPerMonth as $month)
+                    {{$month['month'].','}}
+                @endforeach
+            ],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+    </script>
+        
+        
+        </div>
+
 
 
 </section>
-
-    <!-- Footer -->
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="jquery/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for this template -->
-    <script src="js/grayscale.min.js"></script>
+    <script src="/jquery/jquery.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/jquery-easing/jquery.easing.min.js"></script>
+    <script src="/js/grayscale.min.js"></script>
   </body>
 <footer class="bg-black small text-center text-white-50">
       <div class="container">
-
         Copyright &copy; Social Pets 2019
       </div>
 </footer>
