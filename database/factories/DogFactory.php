e4a664b8607ff01a12abfe91a99256ca29b1f697
jgonzalezma <jgonzalezma17dw@ikzubirimanteo.com>
2019-01-29 12:46:18 +0100

    push

--- database/factories/DogFactory.php
+++ database/factories/DogFactory.php
@@ -9,5 +9,7 @@ $factory->define(App\Dog::class, function (Faker $faker) {
         'gender' => $faker->numberBetween($min = 0, $max = 1), // secret
         'race' => $faker->name,
         'age' => $faker->numberBetween($min = 1, $max = 10),
+        'lat' => $faker->numberBetween($min = 35.00000000000000, $max = 50.00000000000000),
+        'lng' => $faker->numberBetween($min = -50.00000000000000, $max = 50.00000000000000),
     ];
 });
