0a5676143a32c3c44c7f7ec2c5ad319884f7b233
davidzabalperez <davidzabalperez@gmail.com>
2019-01-31 12:48:57 +0100

    mepesd0nas?

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.feed_master')
+@section('tittle','error 404')
+    
+@section('content')
+    <p></p>
+    <h1 class="text-white">Error 404. Pagina no encontrada </h1>
+    <p></p>
+    <img src="/img/error404.jpg" height="400px" alt="">
+@endsection
