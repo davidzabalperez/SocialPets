e83217a650526e5e9eb0fb31823272431db25066
davidzabalperez <davidzabalperez@gmail.com>
2019-02-07 09:17:21 +0100

    css editar perfil fixed

--- resources/views/editar_perfil/formulario_editar_perfil.blade.php
+++ resources/views/editar_perfil/formulario_editar_perfil.blade.php
@@ -1,9 +1,8 @@
 <div id="editProfileModal" class="modal fade">
     <div class="modal-dialog">
-
     <div class="modal-content">
-      <h4 id="tituloEditarUsuario" class="modal-title">Editar perfil de usuario/perro</h4>
       <div class="modal-header">
+            <h4 id="tituloEditarUsuario" class="modal-title">Editar perfil de usuario/perro</h4>
         <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
       <div class="modal-body">
@@ -17,28 +16,28 @@
       <input type="text" class="form-control" name="name" id="name" value="{{ $user->dog->name }}">
       <input type="hidden" class="form-control" name="id" id="id" value="{{ $user->dog->id }}">
     </div>
-    <button type="submit" class="btn btn-primary">Cambiar nombre</button>
+    <button type="submit" class="btn btn-primary">Cambiar</button>
   </div>
 </form>
   <form action="{{ route('user.update', Auth::user()->id )}}" method="post">
     @method('PATCH')
     @csrf
     <!--NOMBRE -->
-  <div class="form-group row">
-    <label for="inputName3" class="col-sm-2 col-form-label">Nombre</label>
-    <div class="col-6 col-md-6">
-      <input type="text" class="form-control" name="name" id="name" value="{{ Auth::user()->name }}">
+    <div class="form-group row">
+    
+        <div class="col-6 col-md-6">
+                <label for="nombre" class="col-sm-2 col-form-label">Usuario</label>
+        <input type="text" class="form-control" name="name" id="name" value="{{ Auth::user()->name }}">
+        </div>
+    
+        <div class="col-6 col-md-6">
+                <label for="name" class="col-sm-2 col-form-label">Email</label>
+        <input type="email" class="form-control" name="email" id="email" value="{{ Auth::user()->email }}">
+        <p></p>
+        <button type="submit" class="btn btn-primary">Cambiar</button> 
+        </div>
+    
     </div>
-    <button type="submit" class="btn btn-primary">Cambiar nombre</button>
-  </div>
-  <!--EMAIL -->
-  <div class="form-group row">
-    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-6 col-md-6">
-      <input type="text" class="form-control" name="email" id="email" value="{{ Auth::user()->email }}"> 
-    </div>
-    <button type="submit" class="btn btn-primary btn-sm">Cambiar email</button>
-  </div>
 </form>
 <!--PASSWORD -->
 <form method="POST" action="{{ route('changePassword') }}">
@@ -87,58 +86,51 @@
         <form action="{{route('imgur.store')}}" method="post" enctype="multipart/form-data">
             @csrf
             <p><label for="foto"> Foto:</label><input type="file" name="avatar" class="form-control-file"></p>
-            <input type="submit" value="Cambiar imagen" name="submit" class="btn btn-primary btn-sm">
+            <input type="submit" value="Subir imagen" name="submit" class="btn btn-primary btn-sm">
             @if (isset($img))
                 <img src="{{$img}}" alt="avatar">
             @endif
         </form>
-
-          <div class="modal-body">
-                              <br>
-              <button class="btn btn-danger btn-sm" rel="publisher" data-toggle="modal" data-target="#deleteModalUser" id="hideModal">Eliminar usuario</button>
-                     
+        <br>
+            <div class="borrarusuarioperro">
+                    <button class="btn btn-danger btn-sm" rel="publisher" data-toggle="modal" data-target="#deleteModalUser" id="hideModal">Eliminar usuario</button>
+                    <button class="btn btn-danger btn-sm" rel="publisher" data-toggle="modal" data-target="#deleteModalDog" id="hideModal">Eliminar perro</button>       
+            </div>
+            
                           <div class="modal fade" id="deleteModalUser" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                           <div class="modal-dialog">
                               <div class="modal-content">
                                   <div class="modal-footer">
                                       Confirmacion borrar cuenta
                                       <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Cancelar</button>
-                                     
-
                                       <form method="post" action="{{ route('forcedeleteself',$user->id)}}" >
                                           @csrf
                                           <button type="submit" class="btn btn-danger">Borrar usuario</button>
                                       </form>
-                                      
                                   </div>
                               </div>
                           </div>
                       </div>
-          </div>
 
-                    <div class="modal-body">
-                              <br>
-              <button class="btn btn-danger btn-sm" rel="publisher" data-toggle="modal" data-target="#deleteModalDog" id="hideModal">Eliminar perro</button>
                      
-                          <div class="modal fade" id="deleteModalDog" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                          <div class="modal-dialog">
-                              <div class="modal-content">
-                                  <div class="modal-footer">
-                                      Confirmacion borrar perro
-                                      <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Cancelar</button>
-                                     
-
-                                      <form method="post" action="{{ route('dog.destroy',$user->dog->id)}}" >
-                                          @csrf
-                                          @method('DELETE')
-                                          <button type="submit" class="btn btn-danger">Borrar perro</button>
-                                      </form>
-                                      
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-          </div>
+                        <div class="modal fade" id="deleteModalDog" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-footer">
+                                    Confirmacion borrar perro
+                                    <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Cancelar</button>
+                                    
+                                    <form method="post" action="{{ route('dog.destroy',$user->dog->id)}}" >
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Borrar perro</button>
+                                    </form>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+          
       </div>
   </div>
 </div>
