1aec2b39c0d53e1021feecb900f38c49d3796b23
davidzabalperez <davidzabalperez@gmail.com>
2019-02-20 18:05:08 +0100

    reset/password/token ok

--- resources/views/layouts/navbarLoginRegister.blade.php
+++ resources/views/layouts/navbarLoginRegister.blade.php
@@ -1,7 +1,7 @@
     <!-- NavBar -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container-fluid">
-        <a class="navbar-brand js-scroll-trigger" href="/"><img src="../img/logo.png" alt="logo" width="120px;"></a>
+        <a class="navbar-brand js-scroll-trigger" href="/"><img src="/img/logo.png" alt="logo" width="120px;"></a>
       </div>
 
     </nav>
