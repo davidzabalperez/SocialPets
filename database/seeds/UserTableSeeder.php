b1bb773ec2f7e6d42279065089a5155e7b0292c1
Maurii19 <mdiazar17dw@ikzubirimanteo.com>
2019-02-21 23:22:34 +0100

    Error 404 ;)

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,7 +12,7 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'id' => '1',
+            'id' => 1,
             'name' => 'user',
             'email' => 'user@user.com',
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
@@ -20,7 +20,7 @@ class UserTableSeeder extends Seeder
           	'role' => 'user'
         ]);
         DB::table('users')->insert([
-            'id' => '2',
+            'id' => 2,
             'name' => 'Paulo',
             'email' => 'Paulo@gmail.com',
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
@@ -28,7 +28,7 @@ class UserTableSeeder extends Seeder
               'role' => 'user'
         ]);
         DB::table('users')->insert([
-            'id' => '3',
+            'id' => 3,
             'name' => 'Eneko',
             'email' => 'Eneko@gmail.com',
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
@@ -36,7 +36,7 @@ class UserTableSeeder extends Seeder
               'role' => 'user'
         ]);
         DB::table('users')->insert([
-            'id' => '4',
+            'id' => 4,
             'name' => 'Juan',
             'email' => 'Juan@gmail.com',
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
@@ -44,7 +44,7 @@ class UserTableSeeder extends Seeder
               'role' => 'user'
         ]);
         DB::table('users')->insert([
-            'id' => '5',
+            'id' => 5,
             'name' => 'Robert',
             'email' => 'Robert@gmail.com',
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
@@ -52,12 +52,12 @@ class UserTableSeeder extends Seeder
               'role' => 'user'
         ]);
         DB::table('users')->insert([
-            'id' => '6',
+            'id' => 6,
             'name' => 'Aritz',
             'email' => 'Aritz@gmail.com',
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
             'email_verified_at' => '2019-02-04 07:11:52',
-              'role' => 'user'
+              'role' => 'admin'
         ]);
     }
 }
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,13 +2,23 @@
 @section('tittle','error 404')
     
 @section('content')
+
+
+
+<audio autoplay="autoplay" style="display:none;">
+<source src="/sounds/404.mp3" type="audio/mpeg">
+<embed src="/sounds/404.mp3" hidden="true" autostart="true" loop="false" class="playSound" />
+</audio>
+
     <div class="container">
         <div class="row">
             <div class="col-xs-1-12">
-                    <h1 class="h1white">Error 404. Pagina no encontrada </h1>
+            <center><h1 class="h1white">Error 404. Pagina no encontrada </h1>
                     <br>
-                <img src="/img/error404.jpg" height="400px" alt="">                
+               <img src="/img/error404.jpg" height="400px" alt=""></center>         
             </div>
         </div>
     </div>
 @endsection
+
+
