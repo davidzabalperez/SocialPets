fa53c5fd6d62052699848f23153843cd6239d7ce
Maurii19 <mdiazar17dw@ikzubirimanteo.com>
2019-02-21 22:27:55 +0100

    prueba

--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -12,7 +12,7 @@ class AdminTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'id' => '0',
+            'id' => 0,
             'name' => 'admin',
             'email' => 'admin@admin.com',
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
